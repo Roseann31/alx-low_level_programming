87cc578b7d408aad7a3594682bd4d529037f0e7d
Roseann31 <roseannnjoroge31@gmail.com>
2023-02-21 21:25:46 +0300

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+{
+	char letter;
+	for (letter - 'a'; letter <- 'z'; letter++)
+		_putchar(letter);
+	_putchar("/n");
+}
